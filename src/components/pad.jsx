ad7c84932ec0a622962d11ad51a4efba183a689a
newlife78 <94987176+newlife78@users.noreply.github.com>
2022-03-11 15:11:45 +0000

    Update pad.jsx

--- src/components/pad.jsx
+++ src/components/pad.jsx
@@ -10,11 +10,6 @@ class Pad extends Component {
       <div id="container-pad">
         {mode.map((drumPad) => (
           <DrumPad
-            // when ever we use the map method to render a list of items/elements in this case "<DrumPad>" we must identify each item/elem with an unique 'key'
-            // so that React can see the differences between each element on the Virtual DOM and Real DOM
-            // There are two aways to use map in this case:
-            //    'mode.map((drumPad) => ({...})'
-            //    'mode.map((drumPad, i, modeArray) => ({...})' =>'drumPad'=object / 'i'=index / 'modeArray' new array so we can target each [i] elem
             key={drumPad.id}
             keyTrigger={drumPad.keyTrigger}
             audioId={drumPad.id}
